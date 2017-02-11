d71babb61a593a2c352e82d46ccc0e114691a62a
andrese <andreseloysv@gmail.com>
2017-02-11 13:00:23 +0100

    Scene Mesches folder

---
+++ Assets/Scripst/scena/sceneController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour {
+	List<string> scenes = new List<string>();
+	string currentScene = "";
+	int currentSceneIndex;
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+		loadScenes();
+		currentSceneIndex = 0;
+		currentScene = scenes[currentSceneIndex];
+    }
+
+	void Update()
+	{
+		if(currentSceneIndex == 0){
+			if (Input.GetButtonDown("Submit")){
+				currentSceneIndex++;
+				Debug.Log(scenes[currentSceneIndex]);
+				SceneManager.LoadScene (scenes[currentSceneIndex], LoadSceneMode.Single);
+			}
+		}
+		
+	}
+	void loadScenes(){
+		scenes.Add("welcome");
+		scenes.Add("level1");	
+	}
+}
