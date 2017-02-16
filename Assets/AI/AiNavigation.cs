91f157e6323b567b9899633335b2b10c43d969d9
Carlos (Adsith) <carlosalvarez1512@gmail.com>
2017-02-16 22:26:23 +0100

    -Move Scene and code from the other branch

---
+++ Assets/AI/AiNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiNavigation : MonoBehaviour {
+
+    public NavMeshAgent navMeshAgent;
+    public Transform target;
+
+    // Use this for initialization
+    void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+	}
+}
